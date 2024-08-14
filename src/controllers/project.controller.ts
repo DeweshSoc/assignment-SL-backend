95fbb5b4cc4fc45f2148eb4e2799539a030fd38b
Dewesh Jha <deweshjha4@gmail.com>
2024-08-14 19:37:04 +0530

    add create/read project controller

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,1 +1,53 @@
-export const projectController = () => {};
+import { NextFunction, Request, Response } from "express";
+import { Project } from "../models";
+import { validateProjectTitle } from "../utils";
+
+
+export const getAllProjects = async (req:Request,res:Response,next:NextFunction) => {
+    try{
+
+        const {user} = req.body;
+        const projects = await Project.find({user:user._id});
+        return res.status(200).json({
+            data:{
+                projects,
+                message:`Fetched ${projects.length} projects`
+            }
+        });
+
+    }catch(err){
+        next(err);
+    }
+}
+
+
+export const createProjectController = async (req:Request,res:Response,next:NextFunction) => {
+
+    try{
+        const { user, projectTitle } = req.body;
+        
+        const duplicateProject = await Project.findOne({ title: projectTitle });
+        if (duplicateProject) {
+            return res.status(400).json({ message: "Duplicate Project" });
+        }
+
+        if (!validateProjectTitle(projectTitle)) {
+            return res.status(400).json({ message: "Invalid project title" });
+        }
+
+        const newProj = new Project({
+            user: user._id,
+            title: projectTitle,
+            episodes: []
+        });
+
+        await newProj.save();    
+        return res.status(200).json({
+            message:"Project created successfully"
+        })
+
+    }catch(err){
+        next(err);
+    }
+};
+
