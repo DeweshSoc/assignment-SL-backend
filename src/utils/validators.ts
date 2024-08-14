6e3c193343c2ad12860d2b82bee30640c3e1f2dd
Dewesh Jha <deweshjha4@gmail.com>
2024-08-14 12:23:42 +0530

    add project title validator

---
+++ src/utils/validators.ts
@@ -0,0 +1,10 @@
+export const validateProjectTitle = (title) => {
+    const allowed =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    const trimmedTitle = title.trim();
+    if (trimmedTitle.length < 2) return false;
+    for(let char of trimmedTitle){
+        if(!allowed.split("").includes(char)) return false;
+    }
+    return true
+};
