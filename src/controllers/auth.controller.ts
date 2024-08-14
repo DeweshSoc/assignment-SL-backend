510a357541a43a8476287ab13baf7f58b91cd530
Dewesh Jha <deweshjha4@gmail.com>
2024-08-14 17:20:14 +0530

    move user signup flow to auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,101 @@
-export const authController = () =>{
-    
-}
+import { Request, Response, NextFunction } from "express";
+import isEmail from "validator/lib/isEmail";
+import jwt from "jsonwebtoken";
+
+import { User, Auth } from "../models";
+import moment from "moment";
+
+export const authSignupController = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!isEmail(email)) {
+            return res.json(422).json({
+                message: "Invalid Email",
+            });
+        }
+
+        const newUser = new User({
+            email,
+            password,
+            projects: [],
+        });
+        await newUser.save();
+        res.status(200).json({
+            message: "User created successfully",
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const authLoginController = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { email, password } = req.body;
+        //console.log(email,password);
+
+        if (!isEmail(email)) {
+            return res.json(422).json({
+                message: "Invalid Email",
+            });
+        }
+
+        const user = await User.findOne({
+            email
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+
+        const isMatch = await user.comparePasswords(password);
+        //console.log(isMatch);
+        if (!isMatch) {
+            return res.status(403).json({
+                message: "Invalid Password",
+            });
+        }
+
+        const duplicate = await Auth.findOne({user:user._id});
+        if (duplicate && moment(duplicate.expires).isAfter(moment(Date.now()))) {
+            return res.status(200).json({
+                message:"Already logged in"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                email: user.email
+            },
+            process.env.JWT_SECRET as string,
+            {
+                expiresIn: "2 days",
+            }
+        );
+
+        const newAuth = new Auth({
+            user: user._id,
+            token,
+            expires: new Date(moment().add(2,'days').toString())
+        });
+
+        await newAuth.save();
+
+        res.status(200).json({
+            message: "Login Successful",
+        });
+    } catch (err) {
+        next(err);
+    }
+};
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,27 +1,4 @@
 import { Request, Response, NextFunction } from "express";
 import isEmail from "validator/lib/isEmail";
-import User from "../models/user.model";
+import {User} from "../models/user.model";
 
-export const createUserController = async (req: Request,res: Response,next: NextFunction) => {
-    try{
-        const {email, password} = req.body;
-
-        if(!isEmail(email)){
-            return res.send(422).send({
-                message:"Invalid Email"
-            })
-        }
-
-        const newUser = new User({
-            email,
-            password,
-            projects:[]
-        })
-        await newUser.save();
-        res.status(200).send({
-            message:"User created successfully"
-        })
-    }catch(err){
-        next(err);
-    }
-};
