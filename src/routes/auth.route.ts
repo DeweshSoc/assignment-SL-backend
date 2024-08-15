cdf1b335bdece9becbb49a33915f9e04a1b1d521
Dewesh Jha <deweshjha4@gmail.com>
2024-08-15 19:52:05 +0530

    add logout route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { authLoginController, authSignupController } from "../controllers";
+import { authLoginController, authSignupController, logoutController } from "../controllers";
 
 const router = express.Router();
 
 
 router.post("/login",authLoginController)
 router.post("/signup",authSignupController)
+router.get("/logout",logoutController)
 
 export const authRoute = router;
