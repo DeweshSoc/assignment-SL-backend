cc00d5bea8029065b57a007314810c3512c480bc
Dewesh Jha <deweshjha4@gmail.com>
2024-08-17 01:09:54 +0530

    add logs for debugging

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -4,19 +4,23 @@ import { Auth } from "../models";
 import moment from "moment";
 
 export async function authenticate(req:Request, res:Response, next:NextFunction){
+    console.log(`INCOMING ===>  ${req.method} - ${req.path}`)
     if(!req.headers.authorization){
+        console.log('No auth header')
         return res.status(403).json({message:"Missing authentication credentials"});
     }
     const token = req.headers.authorization.split(" ")[1];
-
+    
     if(!token){
+        console.log('No Token')
         return res.status(403).json({ message: "Missing authentication credentials" });
     }
 
     try{
         const payload:any = jwt.verify(token,process.env.JWT_SECRET as string);
-        const auth = await Auth.findOne({user:payload.userId},{expires:1});
+        const auth = await Auth.findOne({user:payload.userId},{expireAt:1});
         if(!auth){
+            console.log('No Auth record in db');
             return res.status(403).json({
                 message:"Forbidden"
             })
@@ -24,6 +28,7 @@ export async function authenticate(req:Request, res:Response, next:NextFunction)
         const expireAt = auth?.expireAt;
 
         if(moment(expireAt).isBefore(moment(Date.now()))){
+            console.log('token expired');
             return res.status(403).json({message:"Forbidden"});
         }
 
